42780be2a857bb4816124ea32829a3134e820203
Rafael Naranjo <naranara@espol.edu.ec>
2022-09-05 23:52:47 -0500

    Mejora del print() de los resultados

--- Balanceo.py
+++ Balanceo.py
@@ -21,7 +21,7 @@ f1 = float(input('   magnitud (lb): '))
 print("\nFuerza Desbalanceada 2: ")
 f2 = float(input('   magnitud (lb): '))
 θ2 = float(input('   ángulo (°): '))
-Y = float(input('\nPeso de Balanceo (lb): \n'))
+Y = float(input('\nPeso de Balanceo (lb): '))
 
 m2 = m3 = Y/g
 e2 = 0
@@ -33,13 +33,19 @@ m3_ye3_y = (-(f1 * math.sin(θ1)*l12) - (f2*math.sin(θ2)*l42)) / (l32*w**2)
 θ3 = math.atan(m3_ye3_y/m3_xe3_x)*(180/math.pi)
 m3e3 = math.sqrt((m3_xe3_x**2)+(m3_ye3_y**2))
 e3 = m3e3/m3
-print('θ3: ',θ3,'°')
-print('e3: ',e3,'pulg')
 
 m2_xe2_x = (-f1 * math.cos(θ1) - f2*math.cos(θ2) - m3_xe3_x*w**2) / w**2
 m2_ye2_y = (-f1 * math.sin(θ1) - f2*math.sin(θ2) - m3_ye3_y*w**2) / w**2
 θ2 = math.atan(m2_ye2_y/m2_xe2_x)*(180/math.pi)
 m2e2 = math.sqrt((m2_xe2_x**2)+(m2_ye2_y**2))
 e2 = m2e2/m2
-print('θ2: ',θ2,'°')
-print('e2: ',e2,'pulg')
+
+print('\n{:-^25s}\n'.format('RESULTADOS'))
+
+print('\n{:_^25s}\n'.format('Disco 3'))
+print(' θ3: {:5.2f}°'.format(θ3))
+print(' e3: {:5.3f} pulg'.format(e3))
+
+print('\n{:_^25s}\n'.format('Disco 2'))
+print(' θ2: {:5.2f}°'.format(θ2))
+print(' e2: {:5.3f} pulg'.format(e2))
